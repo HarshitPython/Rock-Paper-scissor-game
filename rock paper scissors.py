06c48012bc6ec661ca1624be2928e0649cf0df04
HarshitPython <88823245+HarshitPython@users.noreply.github.com>
2021-08-15 13:31:18 +0530

    Add files via upload

---
+++ rock paper scissors.py
@@ -0,0 +1,68 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+
+
+
+
+
+import random
+
+a = int(input('What do you choose ?"0" for rock, "1" for paper, "2" for scissors \n'))
+
+if a == 0:
+    print(rock)
+elif a == 1:
+    print(paper)
+elif a == 2:
+    print(scissors)
+
+b = random.randint(0,2)
+print(f"Computer choose: \n {b}")
+
+if b==0:
+    print(rock)
+elif b==1:
+    print(paper)
+else:
+    print(scissors)
+
+if(a == 0) and (b == 1):
+    print("You Loose")
+elif(a == 0) and (b == 2):
+    print("You Win")
+elif(a == 1) and (b == 0):
+    print("You Win")
+elif(a == 1) and(b == 2):
+    print("You Loose")
+elif(a == 2) and (b == 0):
+    print("You Loose")
+elif(a == 2) and (b == 1):
+    print("You Win")
+else:
+    print("Draw")
+    
